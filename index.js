46f7540bbfaabbc33f106361ec2035773252b01b
Toavina Sylvianno <120978827+ToavinaJr@users.noreply.github.com>
2023-08-06 23:26:12 +0300

    squeletons; html,  css, js

---
+++ index.js
@@ -0,0 +1,35 @@
+let inputTodo = document.getElementById('input-todo');
+let btnTodo = document.getElementById('btn-todo');
+let main = document.getElementById('main');
+
+btnTodo.addEventListener('click' , addTask);
+
+function addTask() {
+      let task = document.createElement('div');
+      task.classList.add("task");
+
+      let title = document.createElement('div');
+      title.classList.add("title");
+      title.textContent = inputTodo.value;
+      inputTodo.value = ""
+
+      let del = document.createElement('div');
+      del.classList.add('delete');
+      del.textContent = 'Delete';
+
+      let edit = document.createElement('div');
+      edit.classList.add('edit');
+      edit.textContent = 'Edit';
+
+      task.append(title, del, edit);
+      main.append(task);
+
+      del.addEventListener('click', () => {
+            main.removeChild(task)
+      })
+
+      edit.addEventListener('click', () => {
+            const modif = prompt('EDit your task ');
+            title.textContent = modif;
+      })
+}
