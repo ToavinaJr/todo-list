0b1671e435101967db0a378b2a872d24b2c6a4b1
Toavina Sylvianno <120978827+ToavinaJr@users.noreply.github.com>
2023-08-07 16:47:21 +0300

    input filter for an invalid task name

--- index.js
+++ index.js
@@ -6,36 +6,43 @@ btnTodo.addEventListener('click' , addTask);
 
 
 function addTask() {
-      let task = document.createElement('div');
-      task.classList.add("task");
-
-      let title = document.createElement('div');
-      title.classList.add("title");
-      title.textContent = inputTodo.value;
-      inputTodo.value = ""
-
-      let del = document.createElement('div');
-      del.classList.add('delete');
-      del.textContent = 'Delete';
-
-      let edit = document.createElement('div');
-      edit.classList.add('edit');
-      edit.textContent = 'Edit';
-
-      task.append(title, del, edit);
-      main.append(task);
-
-      del.addEventListener('click', () => {
-            main.removeChild(task)
-      })
-
-      edit.addEventListener('click', () => {
-            let modif = prompt('EDit your task ');
-            while (modif==="") {
-                  modif = prompt('EDit your task ');
-            }
-            title.textContent = modif;
-      })
+      if (inputTodo.value !== "" && inputTodo.value.trim() !== "") {
+            let task = document.createElement('div');
+            task.classList.add("task");
+      
+            let title = document.createElement('div');
+            title.classList.add("title");
+            title.textContent = inputTodo.value.trim();
+            inputTodo.value = ""
+      
+            let del = document.createElement('div');
+            del.classList.add('delete');
+            del.textContent = 'Delete';
+      
+            let edit = document.createElement('div');
+            edit.classList.add('edit');
+            edit.textContent = 'Edit';
+      
+            task.append(title, del, edit);
+            main.append(task);
+      
+            del.addEventListener('click', () => {
+                  main.removeChild(task)
+            })
+      
+            edit.addEventListener('click', () => {
+                  let modif = prompt('Edit your task name');
+      
+                  while (modif === "") {
+                        modif = prompt('Edit your task name ');
+                  }
+      
+                  title.textContent = modif;
+            })
+      }
+      else {
+            inputTodo.value = "";
+      }   
 }
 
 document.getElementById("erase").addEventListener('click', () => {
